c7a098d6106352f7757d4e8c03a74c6583a84e14
sinso <sofsin31@gmail.com>
2023-10-27 11:20:44 +0600

    ver 1

---
+++ views/layouts/main.php
@@ -0,0 +1,143 @@
+<?php
+
+/** @var yii\web\View $this */
+/** @var string $content */
+
+use app\assets\AppAsset;
+use app\widgets\Alert;
+
+use yii\bootstrap5\Breadcrumbs;
+use yii\bootstrap5\Html;
+use yii\bootstrap5\Nav;
+use yii\bootstrap5\NavBar;
+use yii\helpers\Url;
+
+AppAsset::register($this);
+$asset = AppAsset::register($this);
+$this->registerCsrfMetaTags();
+$this->registerMetaTag(['charset' => Yii::$app->charset], 'charset');
+$this->registerMetaTag(['name' => 'viewport', 'content' => 'width=device-width, initial-scale=1, shrink-to-fit=no']);
+$this->registerMetaTag(['name' => 'description', 'content' => $this->params['meta_description'] ?? '']);
+$this->registerMetaTag(['name' => 'keywords', 'content' => $this->params['meta_keywords'] ?? '']);
+$this->registerLinkTag(['rel' => 'icon', 'type' => 'image/x-icon', 'href' => Yii::getAlias('@web/favicon.ico')]);
+?>
+<?php $this->beginPage() ?>
+<!DOCTYPE html>
+<html lang="<?= Yii::$app->language ?>" class="h-100">
+<head>
+    <title><?= Html::encode($this->title) ?></title>
+    <?php $this->head() ?>
+</head>
+<body class="d-flex flex-column h-100">
+<?php $this->beginBody() ?>
+
+<!--<header id="header">-->
+<!--    --><?php
+//    NavBar::begin([
+//        'brandLabel' => Yii::$app->name,
+//        'brandUrl' => Yii::$app->homeUrl,
+//        'options' => ['class' => 'navbar-expand-md navbar-dark bg-dark fixed-top']
+//    ]);
+//    echo Nav::widget([
+//        'options' => ['class' => 'navbar-nav'],
+//        'items' => [
+//            ['label' => 'Home', 'url' => ['/site/index']],
+//            ['label' => 'About', 'url' => ['/site/about']],
+//            ['label' => 'Contact', 'url' => ['/site/contact']],
+//            Yii::$app->user->isGuest
+//                ? ['label' => 'Login', 'url' => ['/site/login']]
+//                : '<li class="nav-item">'
+//                    . Html::beginForm(['/site/logout'])
+//                    . Html::submitButton(
+//                        'Logout (' . Yii::$app->user->identity->username . ')',
+//                        ['class' => 'nav-link btn btn-link logout']
+//                    )
+//                    . Html::endForm()
+//                    . '</li>'
+//        ]
+//    ]);
+//    NavBar::end();
+//    ?>
+<!--</header>-->
+<header>
+
+
+    <nav id="nav_dark" class="navbar border-bottom border-body text-light" data-bs-theme="dark">
+        <div class="container-fluid">
+            <form id="search_dark" class="d-flex" role="search">
+                <input class="form-control me-2" type="search" placeholder="Поиск..." aria-label="Search">
+            </form>
+            <p>Бьюти-бокс месяца стоимостью 2500 руб. всего за 1000 руб.</p>
+            <a class="navbar-brand" href="#">
+                <img src="<?= $asset->baseUrl.'/img/ph.JPG'?>" alt="Logo" width="47" height="42" class="d-inline-block align-text-top">
+                Вход
+            </a>
+
+        </div>
+    </nav>
+
+
+    <nav id="nav_white" class="navbar">
+        <div class="container-fluid">
+            <p class="text-start">
+                <a class="navbar-brand" href="<?= Url::to(['podpiska'])?>">Боксы по подписке</a>
+                <a class="navbar-brand" href="#cont">Контакты</a>
+            </p>
+            <a class="navbar-brand" href="<?= Url::to(['index'])?>">
+                <img src="<?= $asset->baseUrl.'/img/ph1.JPG'?>" alt="Bootstrap" width="224" height="76">
+            </a>
+            <a class="navbar-brand" href="<?= Url::to(['basket'])?>">Корзина(0)</a>
+
+        </div>
+    </nav>
+</header>
+<main id="main" class="flex-shrink-0" role="main">
+    <div class="container">
+        <?php if (!empty($this->params['breadcrumbs'])): ?>
+            <?= Breadcrumbs::widget(['links' => $this->params['breadcrumbs']]) ?>
+        <?php endif ?>
+        <?= Alert::widget() ?>
+        <?= $content ?>
+    </div>
+</main>
+<div id="footer_or" class="text-start row row-cols-1 row-cols-md-3 g-4 align-items-center">
+    <div id="f1">
+        <h3>Магазин</h3><br>
+        <a href="<?= Url::to(['podpiska'])?>">Подписка на боксы</a><br>
+        <a href="<?= Url::to(['dostavka'])?>">Доставка и возврат</a><br>
+        <a href="<?= Url::to(['politics'])?>">Политика магазина</a><br>
+        <a href="<?= Url::to(['politics', '#'=>'sposob'])?>">Способы оплаты</a><br>
+        <a href="<?= Url::to(['qa'])?>">Вопросы и ответы</a><br>
+    </div>
+    <div id="f2" class="text-center">
+        <h2>Коробочки</h2>
+        <p>Подпишитесь для обновлений и уведомлениях об ограниченных сериях</p>
+        <input type="text" class="form-control" id="specificSizeInputName" placeholder="Эл. почта*">
+        <button type="submit" class="btn btn-dark"><img src="<?= $asset->baseUrl.'/img/bt1.JPG'?>"></button>
+        <br><br>
+    </div>
+    <div id="f3" class="text-start">
+        <p><a name="cont"></a></p>
+        <h3>Контакты</h3><br>
+        <p>ул. Арбат, 1а,  Москва,<br>
+            119019, Россия<br>
+            info@mysite.ru<br>
+            +7 123 456-78-90</p><br>
+    </div>
+</div>
+
+<!--основной футер-->
+<footer class="text-center">Created by Sinitsina Sofiya</footer>
+<!--<footer id="footer" class="mt-auto py-3 bg-light">-->
+<!--    <div class="container">-->
+<!--        <div class="row text-muted">-->
+<!--            <div class="col-md-6 text-center text-md-start">&copy; My Company --><?php //= date('Y') ?><!--</div>-->
+<!--            <div class="col-md-6 text-center text-md-end">--><?php //= Yii::powered() ?><!--</div>-->
+<!--        </div>-->
+<!--    </div>-->
+<!--</footer>-->
+
+<?php $this->endBody() ?>
+</body>
+</html>
+<?php $this->endPage() ?>
---
+++ views/site/index.php
@@ -0,0 +1,229 @@
+<?php
+
+/** @var yii\web\View $this */
+
+use app\assets\AppAsset;
+use yii\helpers\Url;
+
+$this->title = 'Boxes';
+?>
+    <div class="carousel-inner">
+        <div class="carousel-item active">
+            <div class="carousel-caption d-none d-md-block text-black text-start position-absolute top-50">
+                <h1>Боксы с доставкой</h1>
+                <h1>прямо к вам домой</h1><br>
+                <p>Это текст. Нажмите здесь, чтобы изменить его. <br> Расскажите посетителям сайта о себе</p><br>
+                <button type="button" class="btn btn-dark"><a href="<?= Url::to(['podpiska'])?>" id="first_page_buttons">Заказать коробочки</a></button>
+            </div>
+            <img src="https://static.wixstatic.com/media/68c56f_d610707bcc8048b589d1e76d1bc30149~mv2.jpg/v1/fill/w_1349,h_812,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/68c56f_d610707bcc8048b589d1e76d1bc30149~mv2.jpg" class="d-block w-100" alt="...">
+        </div>
+    </div>
+
+    <!--бренды-->
+    <div id="carouselExampleIndicators" class="carousel slide">
+        <h1 class="text-center">Бренды</h1>
+        <div class="carousel-indicators">
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+        </div>
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="<?= AppAsset::register($this)->baseUrl.'/img/br1.JPG'?>" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="<?= AppAsset::register($this)->baseUrl.'/img/br2.JPG'?>" class="d-block w-100" alt="...">
+            </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
+    </div>
+
+    <h1 class="text-center">Как мы работаем</h1><br>
+
+    <!--три этапа как мы работаем-->
+    <div class="row row-cols-1 row-cols-md-3 g-4">
+        <div class="col">
+            <div class="card h-100 text-center">
+                <img src="<?= AppAsset::register($this)->baseUrl.'/img/car1.JPG'?>" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h4 class="card-title">Выберите бокс</h4>
+                    <h5 class="card-text">Это текст. Нажмите здесь, чтобы изменить его.</h5>
+                </div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="card h-100 text-center">
+                <img src="<?= AppAsset::register($this)->baseUrl.'/img/car2.JPG'?>" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h4 class="card-title">Выберите подписку</h4>
+                    <h5 class="card-text">Это текст. Нажмите здесь, чтобы изменить его.</h5>
+                </div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="card h-100 text-center">
+                <img src="<?= AppAsset::register($this)->baseUrl.'/img/car3.JPG'?>" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h4 class="card-title">Доставка раз в месяц</h4>
+                    <h5 class="card-text">Это текст. Нажмите здесь, чтобы изменить его.</h5>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!--про упаковки-->
+    <div class="text-center">
+        <button type="button" class="btn btn-dark"><a href="<?= Url::to(['podpiska'])?>" id="first_page_buttons">Заказать коробочки</a></button>
+        <h1>Наши упаковки</h1>
+        <button type="button" class="btn btn-dark"><a href="<?= Url::to(['podpiska'])?>" id="first_page_buttons">Заказать коробочки</a></button>
+        <br><br><br>
+    </div>
+<!--<div class="site-index">-->
+<!---->
+<!--    <div class="jumbotron text-center bg-transparent mt-5 mb-5">-->
+<!--        <h1 class="display-4">Congratulations!</h1>-->
+<!---->
+<!--        <p class="lead">You have successfully created your Yii-powered application.</p>-->
+<!---->
+<!--        <p><a class="btn btn-lg btn-success" href="https://www.yiiframework.com">Get started with Yii</a></p>-->
+<!--    </div>-->
+<!---->
+<!--    <div class="body-content">-->
+<!---->
+<!--        <div class="row">-->
+<!--            <div class="col-lg-4 mb-3">-->
+<!--                <h2>Heading</h2>-->
+<!---->
+<!--                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et-->
+<!--                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip-->
+<!--                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu-->
+<!--                    fugiat nulla pariatur.</p>-->
+<!---->
+<!--                <p><a class="btn btn-outline-secondary" href="https://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>-->
+<!--            </div>-->
+<!--            <div class="col-lg-4 mb-3">-->
+<!--                <h2>Heading</h2>-->
+<!---->
+<!--                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et-->
+<!--                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip-->
+<!--                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu-->
+<!--                    fugiat nulla pariatur.</p>-->
+<!---->
+<!--                <p><a class="btn btn-outline-secondary" href="https://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>-->
+<!--            </div>-->
+<!--            <div class="col-lg-4">-->
+<!--                <h2>Heading</h2>-->
+<!---->
+<!--                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et-->
+<!--                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip-->
+<!--                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu-->
+<!--                    fugiat nulla pariatur.</p>-->
+<!---->
+<!--                <p><a class="btn btn-outline-secondary" href="https://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>-->
+<!--            </div>-->
+<!--        </div>-->
+<!---->
+<!--    </div>-->
+<!--</div>-->
+<!---->
+<!---->
+<?php
+//$word1 = 'idealpad';
+//$arr = [];
+//$k = 0;
+//for($i=0; $i<strlen($word1); $i++) {
+//    if (str_contains($word1[$i], 'i')||str_contains($word1[$i], 'a')||str_contains($word1[$i], 'e')||str_contains($word1[$i], 'u')||str_contains($word1[$i], 'y')||str_contains($word1[$i], 'o')){
+//$arr[$k]=$word1[$i];
+//$k++;
+//    }}
+//$rev = array_reverse($arr);
+//echo (implode(" ",$rev));
+//
+//?>
+<!---->
+<?php
+//$numb = array(5,5,5,4,5,3,5,4,2,3,5);
+//$res = [
+//        "1" => 0,
+//    "2" => 0,
+//    "3" => 0,
+//    "4" => 0,
+//    "5" => 0,
+//];
+//for ($i = 0; $i<sizeof($numb); $i++){
+//    switch ($numb[$i]){
+//        case 1:
+//            $res[1]++;
+//            break;
+//        case 2:
+//            $res[2]++;
+//            break;
+//        case 3:
+//            $res[3]++;
+//            break;
+//        case 4:
+//            $res[4]++;
+//            break;
+//        case 5:
+//            $res[5]++;
+//            break;
+//        default: break;
+//    }
+//}
+//print_r($res);
+//?>
+<!--<br>-->
+<?php
+//$sotn = [];
+//$cr = [];
+//$k=0;
+//
+////creating an array from 30 elements that divided by 3
+//while ($k<=29){
+//    $val = rand(-10000,10000);
+//    if($val%3==0){
+//        $cr[$k]=$val;
+//        $k++;
+//    }
+//}
+//
+////first version
+////for ($i = 0; $i<100; $i++){
+////    $sotn[$i] = rand(-10000,10000);
+////    if($sotn[$i]%3==0 && $k<=29){
+////        $cr[$k]=$sotn[$i];
+////        $k++;
+////    }
+////}
+//
+////bubble sort
+////$temp = $cr[0];
+////for ($i=0; $i < sizeof($cr); $i++){
+////    for ($j=0; $j<sizeof($cr)-$i-1; $j++){
+////        if($cr[$j]>$cr[$j+1]){
+////            $temp = $cr[$j];
+////            $cr[$j]=$cr[$j+1];
+////            $cr[$j+1]=$temp;
+////        }
+////    }
+////}
+//
+//asort($cr,SORT_NUMERIC); //sorting the array
+////var_dump($cr); //output the array
+//
+////var_dump(array_chunk($cr, 3));//implode("<br>",));
+//echo implode("<br>",$cr);
+//echo "<br>"."first part"."<br>";
+//print_r(implode(" ",array_slice($cr, 0, 10, true)));
+//echo "<br>"."second part"."<br>";
+//print_r(implode(" ",array_slice($cr, 10, 10, true)));
+//echo "<br>"."third part"."<br>";
+//print_r(implode(" ",array_slice($cr, 20, 10, true)));
+//
+//?>
